3a5b8cd97aa88dcd013b893af597873f28260a5c
cuonglk1102 <65998576+cuonglk1102@users.noreply.github.com>
2023-11-13 10:25:25 +0700

    update 13/11 v2

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(    express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use(morgan('combined'));
 
 //Template engine
-            app.engine('hbs', exphbs.engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-    app.set('views',    path.join(__dirname, 'resources/views'));
+    app.engine('hbs', exphbs.engine({     extname: '.hbs' }));
+    app.set('view engine', 'hbs');
+    app.set(  'views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
